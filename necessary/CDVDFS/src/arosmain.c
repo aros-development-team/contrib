aros-development-team/contrib

commit b2fd562ab2fabc771b6a585c9f3fadeb02c125a5

Corrected Test_Unit_Ready returning only no-disc status
git-svn-id: https://svn.aros.org/svn/aros/trunk@30803 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

necessary/CDVDFS/src/device.c
@@ -142,6 +142,7 @@
 #include "prefs.h"
 #include "aros_stuff.h"
 
+
 #define ID_CDFS_DISK 0x43444653
 
 /*
@@ -458,7 +459,7 @@ UBYTE   notdone = 1;
 		packet->dp_Res2 = 0;
 		error = 0;	
 		BUG(dbprintf(
-			"Packet: %3ld %08lx %08lx %08lx %10s ",
+			"[CDVDFS]\tPacket: %3ld %08lx %08lx %08lx %10s ",
 			packet->dp_Type,
 			packet->dp_Arg1,
 			packet->dp_Arg2,
@@ -1476,28 +1477,28 @@ void Unmount (void)
 
 int Mount_Check (void)
 {
-  if (!global->g_disk_inserted) {
-   /*
-    * No disk was inserted up to now: we will check whether
-    * a disk has been inserted by sending the test unit ready
-    * command. We have to send the command twice because
-    * the first SCSI command after inserting a new disk is
-    * always rejected.
-    */
-    if (Test_Unit_Ready (global->g_cd)) {
-      global->g_disk_inserted = TRUE;
-      Mount ();
-    } else {
-      return 0;
+    D(bug("[CDVDFS]\tMount_Check\n"));
+    if (!global->g_disk_inserted) {
+	/*
+	 * No disk was inserted up to now: we will check whether
+	 * a disk has been inserted by sending the test unit ready
+	 * command. We have to send the command twice because
+	 * the first SCSI command after inserting a new disk is
+	 * always rejected.
+	 */
+	if (0 == Test_Unit_Ready (global->g_cd)) 
+	{
+	    D(bug("[CDVDFS]\tDrive not ready, not mounting\n"));
+	    return 0;
+	}
+
+	D(bug("[CDVDFS]\tDrive ready, mounting disc\n"));
+	global->g_disk_inserted = TRUE;
+	Mount ();
+	return global->DevList ? 1 : 0;
     }
-    if (global->DevList)
-      return 1;
-    else {
-      /* Mount() did not succeed: */
-      return 0;
-    }
-  }
-  return 1;
+    D(bug("[CDVDFS]\tDisc already mounted.\n"));
+    return 1;
 }
 
 /*
@@ -1580,35 +1581,37 @@ void Send_Timer_Request (void)
  *  Check whether the disk has been removed or inserted.
  */
 
-void Check_Disk (void) {
-int i;
-ULONG l1, l2;
+void Check_Disk (void) 
+{
+    int i;
+    ULONG l1, l2;
 
-	BUG(dbprintf ("Checking Disk... ");)
+    BUG(dbprintf ("[CDVDFS]\tChecking Disk... ");)
 	i = Test_Unit_Ready (global->g_cd);
-	l1 = global->g_cd->t_changeint;
-	l2 = global->g_cd->t_changeint2;
-	BUG(if (l1==l2 && i) dbprintf ("no disk change (T %ld)", l1);)
+
+    l1 = global->g_cd->t_changeint;
+    l2 = global->g_cd->t_changeint2;
+    BUG(if (l1==l2 && i) dbprintf ("no disk change (T %ld)", l1);)
 	if (l1!=l2 && i)
 	{
-		global->g_disk_inserted = TRUE;
-		BUG(dbprintf ("disk has been inserted (T %ld)", l1);)
+	    global->g_disk_inserted = TRUE;
+	    BUG(dbprintf ("disk has been inserted (T %ld)", l1);)
 		if (global->DevList)
-			Unmount ();
-		Delay (50);
-		Clear_Sector_Buffers (global->g_cd);
-		Mount ();
+		    Unmount ();
+	    Delay (50);
+	    Clear_Sector_Buffers (global->g_cd);
+	    Mount ();
 	}
-	BUG(if (l1==l2 && !i) dbprintf ("no disk in drive (T %ld)", l1);)
+    BUG(if (l1==l2 && !i) dbprintf ("no disk in drive (T %ld)", l1);)
 	if (l1!=l2 && !i)
 	{
-		global->g_disk_inserted = FALSE;
-		BUG(dbprintf ("disk has been removed (T %ld)", l1);)
+	    global->g_disk_inserted = FALSE;
+	    BUG(dbprintf ("disk has been removed (T %ld)", l1);)
 		global->playing = FALSE;
-		Unmount ();
-		global->g_cd->t_changeint2 = global->g_cd->t_changeint;
+	    Unmount ();
+	    global->g_cd->t_changeint2 = global->g_cd->t_changeint;
 	}
-	BUG(dbprintf ("\n");)
+    BUG(dbprintf ("\n");)
 }
 
 /* The following lines will generate a `disk inserted/removed' event, in order

necessary/CDVDFS/src/iso9660.c
@@ -115,14 +115,16 @@ int Get_File_Name(VOLUME *volume, directory_record *dir, char *buf, int buflen)
 	    break;
 
 	case PRO_JOLIET:
-	    return Get_Joliet_Name(dir->file_id, buf, dir->file_id_length);
+	    len = Get_Joliet_Name(dir->file_id, buf, dir->file_id_length);
+	    break;
 
 	default:
+	    len = dir->file_id_length;
+	    CopyMem(dir->file_id, buf, len);
 	    break;
     }
-    CopyMem(dir->file_id, buf, dir->file_id_length);
-    buf[dir->file_id_length] = 0;
-    return dir->file_id_length;
+    buf[len] = 0;
+    return len;
 }
 
 /* Check whether the given volume uses the ISO 9660 Protocol.

necessary/CDVDFS/src/cdrom.c
@@ -10,6 +10,7 @@
  * ----------------------------------------------------------------------
  * History:
  *
+ * 08-Mar-09 error     - Corrected Test_Unit_Ready returning only NO_DISC state
  * 06-Mar-09 error     - Removed madness, fixed insanity. Cleanup started
  * 06-Jun-08 sonic     - Fixed to compile with gcc v2
  * 30-Mar-08 error     - Updated 'Find_Last_Session' with a generic command
@@ -358,7 +359,7 @@ int Test_Unit_Ready(CDROM *p_cd)
 	(0 != p_cd->scsireq->io_Error))
 	return FALSE;
 
-    return FALSE;
+    return TRUE;
 }
 
 int Mode_Select

necessary/CDVDFS/src/debug.h
@@ -1,6 +1,7 @@
 #ifndef ACDR_DEBUG_H
 #define ACDR_DEBUG_H
 
+#define DEBUG 0
 #if defined(__AROS__) || defined(__MORPHOS__)
 #ifdef __AROS__
 #	include <aros/debug.h>

necessary/CDVDFS/src/devsupp.c
@@ -398,8 +398,6 @@ int Handle_Control_Packet (ULONG p_type, ULONG p_par1, ULONG p_par2)
   return 0;
 }
 
-#if !defined(NDEBUG) || defined(DEBUG_SECTORS)
-
 char *typetostr (int ty)
 {
     switch(ty) {
@@ -580,4 +578,3 @@ int __fflush(void)
 }
 
 #endif /* !(__AROS__ || __MORPHOS__) */
-#endif /* !NDEBUG || DEBUG_SECTORS */

necessary/CDVDFS/src/volumes.c
@@ -83,7 +83,7 @@ void Register_Lock (LOCK *p_lock)
   new->next = global->g_lock_list;
   global->g_lock_list = new;
   
-  BUG(dbprintf ("[Installing lock on '%s']", pathname);)
+  BUG(dbprintf ("[CDVDFS]\tInstalling lock on '%s'", pathname);)
 }
 
 /*  Remove the entry for p_lock in the list g_lock_list.

commit 399b416a4950c6095c3f5c326434f656c5d071ee

Removed error in fetching Rock Ridge file names, introduced in r30803. Also fixed a few comment typos.
git-svn-id: https://svn.aros.org/svn/aros/trunk@32625 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

necessary/CDVDFS/src/arosmain.c
@@ -52,8 +52,8 @@ AROS_UFH3(void, ACDROldEntry,
 }
 
 /*
-	every packet style process needs it's own global variable
-	we may loose it on every task switch so set it up here
+	every packet style process needs its own global variable
+	we may lose it on every task switch so set it up here
 	before switching the task
 */
 AROS_UFH1(void, ACDR_Launch,
@@ -771,7 +771,7 @@ D(bug("[acdr] examine: name=%s ([0]=%x)\n", ead->ed_Name, ead->ed_Name[0]));
 #endif
 
             default:
-                D(bug("[acdr] unkown fsa %d\n", iofs->IOFS.io_Command));
+                D(bug("[acdr] unknown fsa %d\n", iofs->IOFS.io_Command));
                 retval = DOSFALSE;
                 error = ERROR_ACTION_NOT_KNOWN;
 		break;

necessary/CDVDFS/src/device.c
@@ -139,7 +139,6 @@
 #include "params.h"
 #include "rock.h"
 #include "globals.h"
-#include "debug.h"
 #include "volumes.h"
 #include "charset.h"
 #include "prefs.h"
@@ -918,7 +917,7 @@ openbreak:
 		case ACTION_INHIBIT:        /* Bool       Bool */
 			if (packet->dp_Arg1 != DOSFALSE)
 			{
-				/* true meens forbid access */
+				/* true means forbid access */
 				global->g_inhibited++;
 				if (global->DevList)
 					Unmount();
@@ -927,7 +926,7 @@ openbreak:
 			}
 			else
 			{
-				/* false meens access allowed */
+				/* false means access allowed */
 				if (global->g_inhibited)
 					global->g_inhibited--;
 				if (global->g_inhibited == 0)

necessary/CDVDFS/src/iso9660.c
@@ -4,13 +4,14 @@
  *
  * ----------------------------------------------------------------------
  * This code is (C) Copyright 1993,1994 by Frank Munkert.
- *              (C) Copyright 2007-2009 The AROS Development Team.
+ *              (C) Copyright 2007-2010 The AROS Development Team.
  * All rights reserved.
  * This software may be freely distributed and redistributed for
  * non-commercial purposes, provided this notice is included.
  * ----------------------------------------------------------------------
  * History:
  * 
+ * 10-Feb-10 neil    - Removed error in fetching Rock Ridge names.
  * 06-Mar-09 error   - Removed madness, fixed insanity. Cleanup started
  * 16-Jun-08 sonic   - Skip files with "Associated" flag set. Helps to
  *                     read ISO discs created under MacOS.
@@ -107,15 +108,16 @@ int Get_File_Name(VOLUME *volume, directory_record *dir, char *buf, int buflen)
 
     switch (volume->protocol) 
     {
+	case PRO_JOLIET:
+	    len = Get_Joliet_Name(dir->file_id, buf, dir->file_id_length);
+	    break;
+
 	case PRO_ROCK:
 	    len = Get_RR_File_Name(volume, dir, buf, buflen);
 	    if (len > 0)
 		return len;
-	    break;
 
-	case PRO_JOLIET:
-	    len = Get_Joliet_Name(dir->file_id, buf, dir->file_id_length);
-	    break;
+	    /* Fall through... */
 
 	default:
 	    len = dir->file_id_length;