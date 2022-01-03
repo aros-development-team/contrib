868bdd18534522aa0e6e2dc19982906de6672db4
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-01-03 13:19:02 +0100

    Don't manually close libraries opened by autoopening
    This was causing MUIMasterBase to be NULL when deleting
    custom classes at CLASSESEXPUNGE

--- workbench/classes/mui/BWins/mcp/libinit_aros.c
+++ workbench/classes/mui/BWins/mcp/libinit_aros.c
@@ -2,21 +2,46 @@
 
 #include <proto/exec.h>
 #include <string.h>
-#include "base.h"
+#include "class.h"
 
 /****************************************************************************/
 
 struct Library         *lib_base;
 
 struct Catalog         *lib_cat = NULL;
-ULONG                  lib_flags = 0;
 
 struct MUI_CustomClass *lib_shape = NULL;
 struct MUI_CustomClass *lib_class = NULL;
 
 /****************************************************************************/
 
-ADD2OPENLIB(initBase, 0);
-ADD2CLOSELIB(freeBase, 0);
+BOOL LibInit(struct Library *lib)
+{
+    lib_base = lib;
+    if (initShape())
+    {
+        initStrings();
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void LibExpunge(struct Library *lib)
+{
+    if (lib_cat)
+    {
+        CloseCatalog(lib_cat);
+        lib_cat = NULL;
+    }
+
+    if (lib_shape)
+    {
+        MUI_DeleteCustomClass(lib_shape);
+        lib_shape = NULL;
+    }
+}
+
+ADD2INITLIB(LibInit, 0);
+ADD2EXPUNGELIB(LibExpunge, 0);
 
 /****************************************************************************/
