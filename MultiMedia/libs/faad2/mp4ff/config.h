9a27096567e4e19a197c658196029760a35f89be
Nick Andrews <kalamatee@gmail.com>
2026-07-21 15:01:35 +0100

    Vendor faad2's mp4ff demuxer as a static linklib and use it to demux and decode AAC tracks from MP4/ISO-BMFF files (.m4a/.m4b, and .aac files as written by ffmpeg without '-f adts'), including the iTunes-style metadata.

---
+++ MultiMedia/libs/faad2/mp4ff/config.h
@@ -0,0 +1,14 @@
+/*
+    Copyright (C) 2026, The AROS Development Team. All rights reserved.
+
+    Minimal build configuration for the vendored mp4ff sources
+    (mp4atom.c includes "config.h" unconditionally on non-Windows).
+*/
+
+#ifndef MP4FF_CONFIG_H
+#define MP4FF_CONFIG_H
+
+#define HAVE_STRING_H 1
+#define HAVE_STDINT_H 1
+
+#endif /* MP4FF_CONFIG_H */
